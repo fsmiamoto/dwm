0bbfd65688280b2330a63372e08444154a7f3582
Francisco Miamoto <fsmiamoto@gmail.com>
2020-04-04 00:30:55 -0300

    add rules for ranger and octave

--- config.h
+++ config.h
@@ -1,7 +1,7 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static const unsigned int borderpx  = 3;        /* border pixel of windows */
+static const unsigned int borderpx  = 2;        /* border pixel of windows */
 static const unsigned int gappx     = 8;        /* gaps between windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
@@ -35,7 +35,8 @@ static const Rule rules[] = {
 	/* class      instance    title       tags mask     isfloating   monitor */
 	{ "Gimp",        NULL,       NULL,       0,            1,           -1 },
 	{ "Firefox",     NULL,       NULL,       1 << 8,       0,           -1 },
-	{ "octave-cli",  NULL,       NULL,       0,            1,           -1 },
+    { NULL,          NULL,       "ranger",   0,            1,           -1 },
+	{ NULL,          NULL,    "octave-cli",  0,            1,           -1 },
 };
 
 /* layout(s) */
@@ -79,6 +80,7 @@ static Key keys[] = {
 	{ MODKEY|ShiftMask,             XK_p,      setgaps,        {.i = 0} },
 	{ MODKEY,                       XK_f,      togglefullscr,  {0} },
 	{ MODKEY,                       XK_F5,     xrdb,           {.v = NULL } },
+	{ MODKEY,                       XK_q,      killclient,     {0} },
 
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
@@ -87,9 +89,6 @@ static Key keys[] = {
 	{ MODKEY,                       XK_h,      setmfact,       {.f = -0.05} },
 	{ MODKEY,                       XK_l,      setmfact,       {.f = +0.05} },
 	{ MODKEY,                       XK_Tab,    view,           {0} },
-	{ MODKEY,                       XK_q,      killclient,     {0} },
-	{ MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} },
-	{ MODKEY,                       XK_f,      setlayout,      {.v = &layouts[1]} },
 	{ MODKEY,                       XK_m,      setlayout,      {.v = &layouts[2]} },
 	{ MODKEY|ShiftMask,             XK_space,  setlayout,      {0} },
 	{ MODKEY,                       XK_space,  togglefloating, {0} },
@@ -111,6 +110,8 @@ static Key keys[] = {
 	TAGKEYS(                        XK_9,                      8)
 	/* { MODKEY,                       XK_d,      incnmaster,     {.i = -1 } }, */
     /* { MODKEY,                       XK_Return, zoom,           {0} }, */
+	/* { MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} }, */
+	/* { MODKEY,                       XK_f,      setlayout,      {.v = &layouts[1]} }, */
 };
 
 /* button definitions */
